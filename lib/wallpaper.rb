d3403a26644540a7d1e85a01daf6c4e50b6ce003
Ruben Espinosa <rderoldan1@gmail.com>
2012-10-09 20:00:56 -0500

    path specified by set_path and stored in YML

--- lib/wallpaper.rb
+++ lib/wallpaper.rb
@@ -1,20 +1,29 @@
+require 'yaml'
 class Wallpaper
+  puts File.dirname(__FILE__)
+  puts "Opening #{@path}"
 
-   def initialize(path= "#{Dir.home}/Pictures/Backgrounds")
-      @path = path
-     puts @path
-   end
+  def self.get_path
+    config = YAML.load_file(File.dirname(__FILE__)+"/config.yml")
+    @path = config["path"]
+  end
 
-   def run
-     files = Dir.entries(@path)
-     files.each do |photo|
+  def self.set_path(path)
+    config = File.open(File.dirname(__FILE__)+"/config.yml",'w')
+    config.write "path: '#{path}'"
+    puts "Configuration file updated"
+    config.close
+  end
 
-       system("gsettings set org.gnome.desktop.background picture-uri 'file:////#{@path}/#{photo}'")
-       sleep(5)
-     end
-   end
+  def self.run
+    path = self.get_path
+    files = Dir.entries(path)
+    files.each do |photo|
+        system("gsettings set org.gnome.desktop.background picture-uri 'file:////#{path}/#{photo}'")
+     sleep(5)
+    end
+  end
 
 end
 
-wallpaper = Wallpaper.new
-wallpaper.run
+
--- wallpaper.gemspec
+++ wallpaper.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name  = "wallpapers"
-  s.version = "0.0.3"
+  s.version = "0.0.4"
   s.date = "2012-10-08"
   s.summary = "Wallpapers!"
   s.description = "A simple way to change the wallpaper image in ubuntu"
